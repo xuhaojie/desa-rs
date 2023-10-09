c4e27cebd9c6401838bad2776043959686974cee
xuhaojie <xuhaojie@hotmail.com>
2023-10-09 18:45:15 +0800

    优化命令提示

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -45,9 +45,11 @@ impl Module for BaseModule {
                 }
             }
         };
+
+        self.command().print_help();
         return Err(io::Error::new(
             io::ErrorKind::Other,
-            format!("require sub command for '{}'", self.name()),
+            format!("Please specify sub command for '{}.'", self.name()),
         ));
     }
 }
@@ -75,32 +77,8 @@ fn main() -> std::io::Result<()> {
         cmd = cmd.subcommand(module.command());
     }
 
-    let matches = cmd.get_matches();
-    /*
-        // 如果用户提供、则获取该值作为config，或者默认使用 “default.conf”
-        let config = matches.value_of("config").unwrap_or("default.conf");
-        println!("Value for config: {}", config);
+    let matches = cmd.clone().get_matches();
 
-        // 在这里调用.unwrap（）是安全的，因为需要“ INPUT”（如果不需要“ INPUT”，
-        // 可以使用 “if let” 有条件地获取值）
-
-        if let Some(input) = matches.value_of("INPUT"){
-            println!("Using input file: {}", input);
-        }
-    */
-    // 根据用户使用“详细”标志的次数来改变输出
-    // (比如 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    /*
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-    */
-
-    // 你可以通过以下方式处理有关子命令的信息：按名称请求它们的匹配（如下所示）
-    // 仅请求正在使用的名称或两者同时请求
     let mut found = false;
     for module in &modules {
         if let Some(matches) = matches.subcommand_matches(module.name()) {
@@ -115,7 +93,7 @@ fn main() -> std::io::Result<()> {
         }
     }
     if !found {
-        println!("please specify a command, for more infomation please type desa --help");
+        cmd.print_help();
     }
     Ok(())
 }
