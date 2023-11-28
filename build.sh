8010b37b56b108ca7302a6b844917b0e10f2c88f
xuhaojie <xuhaojie@hotmail.com>
2023-11-28 14:20:45 +0800

    优化build.sh增加对可执行文件进行压缩的功能

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ export OPENSSL_STATIC=true
 export OPENSSL_DIR=/musl
 
 cargo build --release --target x86_64-unknown-linux-musl
+strip -s ./target/x86_64-unknown-linux-musl/release/desa
+upx --best ./target/x86_64-unknown-linux-musl/release/desa
+ls -hl ./target/x86_64-unknown-linux-musl/release/
